38b6b17ab5d487567cabed9a577895d138d8ca17
Ye Liu <ye@basecase.co>
2016-10-11 00:25:50 -0400

    v0.2.0

--- dist/fetch.json.js
+++ dist/fetch.json.js
@@ -10,6 +10,8 @@ var _isomorphicFetch2 = _interopRequireDefault(_isomorphicFetch);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 var defaultHeaders = {
   'content-type': 'application/json',
   'accept': 'application/json'
@@ -24,7 +26,11 @@ function fetch(url) {
   } : arguments[1];
 
   options.method = (options.method || 'GET').toUpperCase();
-  options.headers = Object.assign({}, headers, options.headers);
+  options.headers = Object.assign({}, headers, options.headers).filter(function (_ref) {
+    var key = _ref.key;
+    var val = _ref.val;
+    return val;
+  });
   if (options.method === 'GET' || options.method === 'HEAD') {
     delete options.body;
   } else if (options.body) {
@@ -73,14 +79,17 @@ fetch.headers = function (_headers) {
   if (!_headers) {
     return headers;
   }
-  var newHeaders = {};
-  Object.keys(_headers).forEach(function (key) {
-    var val = _headers[key];
-    if (val) {
-      newHeaders[key.toLowerCase()] = val;
-    }
+  var newHeaders = _headers.map(function (_ref2) {
+    var key = _ref2.key;
+    var val = _ref2.val;
+    return _defineProperty({}, key.toLowerCase(), val);
   });
   headers = Object.assign({}, defaultHeaders, newHeaders);
+  return fetch;
+};
+
+fetch.clearHeaders = function () {
+  headers = {};
   return fetch;
 };
 
