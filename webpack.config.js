d9bd476dedea4195324ecc17b1c2d9852fc9d291
Ye Liu <ye@basecase.co>
2018-02-02 15:33:52 -0500

    umd build

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+import path from 'path'
+import webpack from 'webpack'
+
+const config = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'fetch.json.js',
+    library: 'fetchJson',
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: 'babel-loader'
+    }]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin()
+  ]
+}
+
+export default config
